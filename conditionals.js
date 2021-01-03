2d009d4d7284f5197ae3d996d607c949597d7db3
JoshuaRully <joshrully@gmail.com>
2021-01-03 23:45:22 +0000

    more conditional exampls

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,14 @@
-//enter temperature below
-const temperature = 15;
+const isCitizen = true;
+const age = 26;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!"); //output if temp is below 0
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!"); //output if temp is below 15 but higher than -1
-} else {
-  console.log("Short sleeves are fine."); //output if temp is 15 or higher
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote");
 }
 
-console.log("Now you're ready to go outside!");
-//all results are a Canadian's perception of what clothing is suitable in these temps., please wear short sleeves in 15deg.C. temps. at yout own risk ;)
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+  
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
